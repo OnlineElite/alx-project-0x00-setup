e5c153c51258d104eb87d9528ab4ed6d5c07da82
OnlineElite <online.0.elite@gmail.com>
2025-07-17 23:49:47 +0100

    update atsk 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,15 @@
 import Card from "@/components/Card"
+import Button from '@/components/Button'
+
 const Landing : React.FC = () => {
     return (
         <div>
             <h1 className="text-xl font-extralight">Landing Page</h1>
+            <div>
+                <Button title="Like" styles="text-sm rounded-sm" />
+                <Button title="Like" styles="text-base rounded-md" />
+                <Button title="Like" styles="text-lg rounded-full" />
+            </div>
             <Card />
             <Card />
             <Card />
