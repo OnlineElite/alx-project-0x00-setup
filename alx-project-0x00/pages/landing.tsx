38ee70a720effe2b8c1937ec55c86e01044bf033
OnlineElite <online.0.elite@gmail.com>
2025-07-17 23:59:50 +0100

    last update tsk6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,21 +9,24 @@ const Landing : React.FC = () => {
             <div className="space-x-4">
                 <Button title="Small sm" styles="text-sm rounded-sm" />
                 <Button title="Small md" styles="text-sm rounded-md" />
-                <Button title="Small full" styles="text-sm rounded-lg" />
+                <Button title="Small lg" styles="text-sm rounded-lg" />
+                <Button title ="Small full" styles="text-sm rounded-full"/>
             </div>
 
             {/* Medium buttons */}
             <div className="space-x-4">
                 <Button title="Medium sm" styles="text-base rounded-sm" />
                 <Button title="Medium md" styles="text-base rounded-md" />
-                <Button title="Medium full" styles="text-base rounded-lg" />
+                <Button title="Medium lg" styles="text-base rounded-lg" />
+                <Button title ="Medium full" styles="text-base rounded-full"/>
             </div>
 
             {/* Large buttons */}
             <div className="space-x-4">
-                <Button title="Large sm" styles="text-lg rounded-sm px-6 py-3" />
-                <Button title="Large md" styles="text-lg rounded-md px-6 py-3" />
-                <Button title="Large full" styles="text-lg rounded-lg px-6 py-3" />
+                <Button title="Large sm" styles="text-lg rounded-sm" />
+                <Button title="Large md" styles="text-lg rounded-md" />
+                <Button title="Large lg" styles="text-lg rounded-lg" />
+                <Button title ="Large full" styles="text-lg rounded-full"/>
             </div>
             <Card />
             <Card />
