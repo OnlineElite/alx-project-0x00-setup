a724dc711105c6734efebd7e51f4709808cfa698
OnlineElite <online.0.elite@gmail.com>
2025-07-17 23:56:03 +0100

    last update of task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,21 +9,21 @@ const Landing : React.FC = () => {
             <div className="space-x-4">
                 <Button title="Small sm" styles="text-sm rounded-sm" />
                 <Button title="Small md" styles="text-sm rounded-md" />
-                <Button title="Small full" styles="text-sm rounded-full" />
+                <Button title="Small full" styles="text-sm rounded-lg" />
             </div>
 
             {/* Medium buttons */}
             <div className="space-x-4">
                 <Button title="Medium sm" styles="text-base rounded-sm" />
                 <Button title="Medium md" styles="text-base rounded-md" />
-                <Button title="Medium full" styles="text-base rounded-full" />
+                <Button title="Medium full" styles="text-base rounded-lg" />
             </div>
 
             {/* Large buttons */}
             <div className="space-x-4">
                 <Button title="Large sm" styles="text-lg rounded-sm px-6 py-3" />
                 <Button title="Large md" styles="text-lg rounded-md px-6 py-3" />
-                <Button title="Large full" styles="text-lg rounded-full px-6 py-3" />
+                <Button title="Large full" styles="text-lg rounded-lg px-6 py-3" />
             </div>
             <Card />
             <Card />
